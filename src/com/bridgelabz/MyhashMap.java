2cb7ecafb499a869374e94a0f97eb546dfeb79d2
Raghu Thilak Gudur Ramadas <thilak.gudur@gmail.com>
2022-09-22 09:42:12 +0530

    Refactored: Remove a word in the hashtable using linkedlist

--- src/com/bridgelabz/MyhashMap.java
+++ src/com/bridgelabz/MyhashMap.java
@@ -3,54 +3,67 @@ package com.bridgelabz;
 import java.util.ArrayList;
 
 public class MyhashMap<K, V> {
-    private final int numBuckets;
-    MyLinkedlist<K, V> myLinkedlist;
-    ArrayList<MyLinkedlist<K, V>> myBucketArray;
-
-    public MyhashMap() {
-        this.numBuckets = 10;
-        this.myBucketArray = new ArrayList<>(numBuckets);
-        this.myLinkedlist = new MyLinkedlist<>();
-        for (int i = 0; i < numBuckets; i++) {
-            this.myBucketArray.add(null);
+    public MyMapNode<K, V> head;
+    public MyMapNode<K, V> search(K key) {
+        MyMapNode<K, V> currnode = head;
+        while (currnode != null) {
+            if (currnode.key.equals(key)) {
+                return currnode;
+            }
+            currnode = currnode.getNext();
         }
+        return null;
+    }
+    public void append(MyMapNode<K, V> myMapNode) {
+        if (head == null) {
+            head = myMapNode;
+            return;
+        }
+        MyMapNode<K, V> currnode = head;
+        while (currnode.next != null) {
+            currnode = currnode.next;
+        }
+        currnode.next = myMapNode;
     }
 
-    private int getBucketIndex(K key) {
-        int hashCode = Math.abs(key.hashCode());
-        int index = hashCode % numBuckets;
-        return index;
-    }
 
     public V get(K key) {
-        int index = this.getBucketIndex(key);
-        MyLinkedlist<K, V> myLinkedlist = this.myBucketArray.get(index);
-        if (myLinkedlist == null) return null;
-        MyMapNode<K, V> myMapNode = (MyMapNode<K, V>) this.myLinkedlist.search(key);
+        MyMapNode<K, V> myMapNode = (MyMapNode<K, V>) this.search(key);
         return (myMapNode == null) ? null : myMapNode.getValue();
 
     }
-
     public void add(K key, V value) {
-        int index = this.getBucketIndex(key);
-        MyLinkedlist<K, V> myLinkedlist = this.myBucketArray.get(index);
-        if (myLinkedlist == null) {
-            myLinkedlist = new MyLinkedlist<>();
-            this.myBucketArray.set(index, myLinkedlist);
-        }
-        MyMapNode<K, V> myMapNode = (MyMapNode<K, V>) this.myLinkedlist.search(key);
+        MyMapNode<K, V> myMapNode = (MyMapNode<K, V>) this.search(key);
         if (myMapNode == null) {
             myMapNode = new MyMapNode<>(key, value);
-            this.myLinkedlist.append(myMapNode);
+            this.append(myMapNode);
         } else {
             myMapNode.setValue(value);
         }
     }
-
+    public void remove(K key) {
+        {
+            MyMapNode currentNode = head;
+            MyMapNode previousNode = null;
+            while (currentNode != null && currentNode.getKey().equals(key)) {
+                head = currentNode.getNext();
+                return;
+            }
+            while (currentNode != null && !(currentNode.getKey().equals(key))) {
+                previousNode = currentNode;
+                currentNode = currentNode.getNext();
+            }
+            if (currentNode != null) {
+                previousNode.next = currentNode.next;
+            }
+            if (currentNode == null)
+                System.out.println(" Word not found! ");
+        }
+    }
     @Override
     public String toString() {
         return "MyhashMap{" +
-                "myLinkedlist=" + myBucketArray +
+                "head=" + head +
                 '}';
     }
 }
